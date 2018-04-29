88db6d8df55f9d693154ef0431ac26ca0529ea25
GeraldFMoraMora <geraldmoramora@gmail.com>
2018-04-29 01:07:35 -0600

    Inicializadas algunas variables y objetos

--- app/src/main/java/com/sportec/sportec/gui/SplashActivity.java
+++ app/src/main/java/com/sportec/sportec/gui/SplashActivity.java
@@ -1,16 +1,24 @@
 package com.sportec.sportec.gui;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.sportec.sportec.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final static int mSplashDelay = 5000;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        this.mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.splash_screen);
 
 
     }
