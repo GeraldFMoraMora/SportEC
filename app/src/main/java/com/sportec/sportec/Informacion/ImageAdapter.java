5681f4f75134f06b10b1c81a0db2e7d8b6540638
GeraldFMoraMora <geraldmoramora@gmail.com>
2018-05-05 11:00:13 -0600

    Grid Finalizado con exito (Grid Deportes)

---
+++ app/src/main/java/com/sportec/sportec/Informacion/ImageAdapter.java
@@ -0,0 +1,69 @@
+package com.sportec.sportec.Informacion;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sportec.sportec.R;
+
+/**
+ * Created by GeraldMM on 05/05/2018.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    public ImageAdapter(Context context){
+        this.mContext=context;
+    }
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        TextView textView;
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.layout_grid_item, parent, false);
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            //imageView.setLayoutParams(new ViewGroup.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //imageView.setPadding(8, 8, 8, 8);
+            textView = new TextView(mContext);
+            textView.setText("gerald");
+        }
+
+        imageView = (ImageView) convertView.findViewById(R.id.imagen_coche);
+        textView = (TextView) convertView.findViewById(R.id.nombre_coche);
+        imageView.setImageResource(mThumbIds[position]);
+        textView.setText(mThumbNames[position]);
+        return convertView;
+    }
+    private Integer[] mThumbIds = {
+            R.mipmap.grid1, R.mipmap.grid2,
+            R.mipmap.grid3, R.mipmap.grid4
+    };
+    private String[] mThumbNames= {
+            "Fútball","Tenis",
+            "Surf","Kayak"
+    };
+}
--- app/src/main/java/com/sportec/sportec/layouts/DeporteLayout.java
+++ app/src/main/java/com/sportec/sportec/layouts/DeporteLayout.java
@@ -13,10 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.sportec.sportec.Informacion.ImageAdapter;
 import com.sportec.sportec.Informacion.Noticia;
 import com.sportec.sportec.Informacion.NoticiaAdapter;
 import com.sportec.sportec.R;
@@ -39,6 +42,17 @@ public class DeporteLayout extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_deporte);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(DeporteLayout.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
     public static Noticia[] ITEMS = {
